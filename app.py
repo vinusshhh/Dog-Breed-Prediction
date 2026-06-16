2c01d744cf5613bb51e177c0a75afa3aa6b08539
Vinush <im.vinush@gmail.com>
2026-06-16 10:02:13 +0530

    Refactor comments and improve code readability

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# ===== IMPORTS (MUST BE AT THE TOP) =====
 import streamlit as st
 import tensorflow as tf
 import json
@@ -6,13 +5,13 @@ import numpy as np
 import requests
 from PIL import Image
 
-# ===== PAGE CONFIG =====
+# Page configuration
 st.set_page_config(page_title="Dog Breed Predictor 🐶", layout="centered")
 
 st.title("🐕 Dog Breed Prediction App")
 st.write("Upload a dog image and the model will predict its breed.")
 
-# ===== DOG API =====
+# API
 API_KEY = "live_1jxGwydpE97RBZgS8clefjOCFVSWqyMCdO7kTOsrytH8NpXDoIm3LZfUb2MD2qKY"
 
 def get_breed_info(breed_name):
@@ -37,24 +36,24 @@ def get_breed_info(breed_name):
 
     return None
 
-# ===== LOAD MODEL =====
+# Load model
 @st.cache_resource
 def load_model():
     return tf.keras.models.load_model("dog_breed_model.keras")
 
 model = load_model()
 
-# ===== LOAD LABELS =====
+# Load label
 with open("class_indices.json") as f:
     class_indices = json.load(f)
 
 labels = {v: k for k, v in class_indices.items()}
 
-# ===== SESSION STATE (RESET) =====
+# Session state
 if "reset" not in st.session_state:
     st.session_state.reset = False
 
-# ===== IMAGE UPLOAD =====
+# Image upload
 uploaded_file = st.file_uploader(
     "Upload a dog image",
     type=["jpg", "jpeg", "png"],
@@ -65,12 +64,12 @@ if uploaded_file is not None and not st.session_state.reset:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # ===== PREPROCESS =====
+    # Preprocess
     img = image.resize((224, 224))
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    # ===== PREDICT =====
+    # Prediction
     prediction = model.predict(img_array)[0]
     class_index = np.argmax(prediction)
     breed = labels[class_index]
@@ -80,7 +79,7 @@ if uploaded_file is not None and not st.session_state.reset:
 
     breed_info = get_breed_info(clean_breed)
 
-    # ===== DISPLAY RESULT =====
+    # Displaying the result
     st.markdown("## 🏆 Predicted Dog Breed")
     st.success(f"**{clean_breed}**")
 
@@ -104,11 +103,11 @@ if uploaded_file is not None and not st.session_state.reset:
         **🏷️ Breed Group:** {breed_info.get('breed_group', 'N/A')}
         """)
 
-    # ===== RESET BUTTON =====
+    # Reset button
     if st.button("🔄 Reset"):
         st.session_state.reset = True
         st.rerun()
 
-# ===== CLEAR STATE AFTER RESET =====
+# Initiate clear after reset
 if st.session_state.reset:
     st.session_state.reset = False
